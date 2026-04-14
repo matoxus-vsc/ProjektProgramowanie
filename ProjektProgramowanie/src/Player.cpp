254827f729726a885b9243e14ccaeaa150ec84f8
matoxus-vsc <miozgamateusz2@gmail.com>
2026-04-14 17:56:44 +0200

    Klasa gracz podstawowaw cpp

---
+++ ProjektProgramowanie/src/Player.cpp
@@ -0,0 +1,26 @@
+#include "Player.h"
+#include <iostream>
+
+Player::Player(int health, int attack)
+    :health(health), attack(attack) {}
+int Player::getHealth() const {
+    return health;
+}
+
+int Player::getAttack() const {
+    return attack;
+}
+
+void Player::takeDamage(int amount) {
+    health -= amount;
+    if (health < 0) health = 0;
+}
+
+bool Player::isAlive() const {
+    return health > 0;
+}
+
+void Player::printStats() const {
+    std::cout << "Player: " << name
+              << " | HP: " << health
+              << " | ATK: " << attack << std::endl;
