d991de35c4bddf122fc118bf35ac6aefaf849d96
matoxus-vsc <miozgamateusz2@gmail.com>
2026-04-14 17:58:03 +0200

    Player Klasa naglowkowy

---
+++ ProjektProgramowanie/include/Player.h
@@ -0,0 +1,23 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+
+class Player {
+public:
+  
+    Player(, int health, int attack);
+
+    int getHealth() const;
+    int getAttack() const;
+
+    void takeDamage(int amount);
+    bool isAlive() const;
+    void printStats() const;
+
+private:
+    int health;
+    int attack;
+};
+
+#endif 
